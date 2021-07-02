b613ca0fdcd5f1198f166230d4a6daead0c954e0
devpelumi <oyelowopelumi@gmail.com>
2021-07-01 20:20:09 -0500

    Added coming soon message to yoruba hyms fav page

--- lib/screens/favourites_screen.dart
+++ lib/screens/favourites_screen.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 import '../providers/english_hymn_provider.dart';
@@ -37,7 +38,36 @@ class Favourites extends StatelessWidget {
         body: TabBarView(
           children: [
             FavoriteTab<EnglishHymnProvider>(),
-            Center(child: Text('Coming soon...')),
+          Padding(
+            padding: const EdgeInsets.fromLTRB(0,100,0,0),
+            child: Column(
+              children: [
+                Center(
+                  child: Container(
+                    height: 220.0,
+                    width: 220.0,
+                    decoration: new BoxDecoration(
+                      shape: BoxShape.circle,
+                      color: Colors.pink.shade100,
+                    ),
+                    child: const Align(
+                      alignment: Alignment.center,
+                      child: Icon(Icons.auto_stories,
+                        size: 60,
+                        color: const Color(0xFFF06292) ,
+                      ),
+                    ),
+                  ),
+                ),
+                SizedBox(height: 40),
+                Text('Coming Soon!',
+                textAlign: TextAlign.center,
+                style: TextStyle(fontFamily: 'Alata',
+                fontSize:22,)
+                  ,)
+              ],
+            ),
+          ),
             FavoriteTab<ResponsiveReadingProvider>(),
           ],
         ),
