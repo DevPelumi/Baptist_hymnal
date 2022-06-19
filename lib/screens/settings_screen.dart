25e4fd3a726dc044f492753ec775be476611cfc8
Owamoyo Evans <miles9morales@gmail.com>
2022-06-19 21:26:05 +0300

    solved settings bug

--- lib/screens/settings_screen.dart
+++ lib/screens/settings_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:settings_ui/settings_ui.dart';
+import 'package:settings_ui/src/utils/theme_provider.dart';
 
 import '../models/settings.dart';
 import '../providers/settings_provider.dart';
@@ -17,87 +18,93 @@ const style = TextStyle(
 class SettingsScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.teal,
-      body: SafeArea(
-        child: Consumer<SettingsProvider>(
-          child: Padding(
-            padding: const EdgeInsets.all(20.0),
-            child: Text(
-              'SETTINGS',
-              style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 23,
-                  fontFamily: 'Alata',
-                  fontWeight: FontWeight.w600),
+    return SettingsTheme(
+      themeData: SettingsThemeData(),
+      platform: DevicePlatform.android,
+      child: Scaffold(
+        backgroundColor: Colors.teal,
+        body: SafeArea(
+          child: Consumer<SettingsProvider>(
+            child: Padding(
+              padding: const EdgeInsets.all(20.0),
+              child: Text(
+                'SETTINGS',
+                style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 23,
+                    fontFamily: 'Alata',
+                    fontWeight: FontWeight.w600),
+              ),
             ),
-          ),
-          builder: (ctx, provider, child) => Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: <Widget>[
-              child,
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Card(
-                  child: SettingsSection(title: null, tiles: [
-                    SettingsTile(
-                      title: Text('Language'),
-                      description: Text(provider.getLanguage().stringValue),
-                      leading: Icon(Icons.language),
+            builder: (ctx, provider, child) {
+              return Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  child,
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Card(
+                      child: SettingsSection(title: null, tiles: [
+                        SettingsTile(
+                          title: Text('Language'),
+                          description: Text(provider.getLanguage().stringValue),
+                          leading: Icon(Icons.language),
+                        ),
+                      ]),
                     ),
-                  ]),
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Card(
-                  child: SettingsSection(title: Container(), tiles: [
-                    SettingsTile.switchTile(
-                      initialValue: provider.isDarkMode(
-                          MediaQuery.of(context).platformBrightness ==
-                              Brightness.dark),
-                      title: Text('Mode'),
-                      description: Text(
-                          '${provider.isDarkMode(MediaQuery.of(context).platformBrightness == Brightness.dark) ? 'Dark' : 'Light'} Mode'),
-                      leading: Icon(Icons.palette),
-                      onToggle: (bool value) async {
-                        await provider.setDarkMode(value);
-                      },
-                    )
-                  ]),
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Card(
-                  child: SettingsSection(title: null, tiles: [
-                    SettingsTile(
-                      title: Text('Font Size'),
-                      description: Text('Hymn font size'),
-                      trailing: DropdownButton(
-                          onChanged: (int newFont) async {
-                            await provider.setFontSize(newFont);
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Card(
+                      child: SettingsSection(title: Container(), tiles: [
+                        SettingsTile.switchTile(
+                          initialValue: provider.isDarkMode(
+                              MediaQuery.of(context).platformBrightness ==
+                                  Brightness.dark),
+                          title: Text('Mode'),
+                          description: Text(
+                              '${provider.isDarkMode(MediaQuery.of(context).platformBrightness == Brightness.dark) ? 'Dark' : 'Light'} Mode'),
+                          leading: Icon(Icons.palette),
+                          onToggle: (bool value) async {
+                            await provider.setDarkMode(value);
                           },
-                          dropdownColor: Colors.green.shade200,
-                          icon: Icon(CupertinoIcons.chevron_down),
-                          value: provider.getFontSize(),
-                          items: [14, 16, 18, 20, 22]
-                              .map((n) => DropdownMenuItem(
-                                  child: Padding(
-                                    padding: const EdgeInsets.symmetric(
-                                        horizontal: 8.0),
-                                    child: Text(n.toString(),
-                                        style: TextStyle(
-                                            fontWeight: FontWeight.bold)),
-                                  ),
-                                  value: n))
-                              .toList()),
-                      leading: Icon(Icons.text_fields),
+                        )
+                      ]),
                     ),
-                  ]),
-                ),
-              ),
-            ],
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Card(
+                      child: SettingsSection(title: null, tiles: [
+                        SettingsTile(
+                          title: Text('Font Size'),
+                          description: Text('Hymn font size'),
+                          trailing: DropdownButton(
+                              onChanged: (int newFont) async {
+                                await provider.setFontSize(newFont);
+                              },
+                              dropdownColor: Colors.green.shade200,
+                              icon: Icon(CupertinoIcons.chevron_down),
+                              value: provider.getFontSize(),
+                              items: [14, 16, 18, 20, 22]
+                                  .map((n) => DropdownMenuItem(
+                                      child: Padding(
+                                        padding: const EdgeInsets.symmetric(
+                                            horizontal: 8.0),
+                                        child: Text(n.toString(),
+                                            style: TextStyle(
+                                                fontWeight: FontWeight.bold)),
+                                      ),
+                                      value: n))
+                                  .toList()),
+                          leading: Icon(Icons.text_fields),
+                        ),
+                      ]),
+                    ),
+                  ),
+                ],
+              );
+            },
           ),
         ),
       ),
